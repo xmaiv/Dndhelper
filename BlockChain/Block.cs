77acb72566fd7ac43314e9f6fea9e2f143e88634
xmaiv <111640278+xmaiv@users.noreply.github.com>
2020-02-09 09:29:53 +0000

    intelli_plug: fix logic error for eco mode profiles

--- BlockChain/Block.cs
+++ BlockChain/Block.cs
@@ -23,3 +23,27 @@ namespace BlockChain
         /// </summary>
         [DataMember]
         public string Data { get; private set; }
+        /// <summary>
+        /// Дата создания блока.
+        /// </summary>
+        [DataMember]
+        public DateTime Created { get; private set; }
+        /// <summary>
+        /// Хэш блока.
+        /// </summary>
+        [DataMember]
+        public string Hash { get; private set; }
+        /// <summary>
+        /// Хэш предыдущего блока.
+        /// </summary>
+        [DataMember]
+        public string PreviousHash { get; private set; }
+        /// <summary>
+        /// Имя пользователя.
+        /// </summary>
+        [DataMember]
+        public string User { get; private set; }
+
+
+        /// <summary>
+        /// Конструктор генезис блока.
