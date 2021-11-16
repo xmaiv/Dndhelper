9c61e59e3eb128584b1fa83c03549b373f229d4b
xmaiv <111640278+xmaiv@users.noreply.github.com>
2021-11-16 13:41:41 +0000

    fix reference error

--- BlockChain/Chain.cs
+++ BlockChain/Chain.cs
@@ -42,4 +42,4 @@ namespace BlockChain
                 }
                 else
                 {
-                    throw new Exception("Ошибка полу
+                    throw new Exception("Ошибка получения блоков из баз
