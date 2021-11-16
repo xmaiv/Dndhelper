6f3ddb29bfc07f20b91875caadc400387c227378
xmaiv <111640278+xmaiv@users.noreply.github.com>
2021-11-16 15:45:35 +0000

    Update the TODO

--- BlockChain/Chain.cs
+++ BlockChain/Chain.cs
@@ -42,4 +42,4 @@ namespace BlockChain
                 }
                 else
                 {
-                    throw new Exception("Ошибка получения блоков из баз
+                    throw new Exception("Ошибка получения блоков из базы данных. Цепочка не п
