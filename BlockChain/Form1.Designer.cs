d2856c11e1698ceca913595827d353b9adcaf5f4
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-04-27 17:11:17 +0000

    v1.13.0

--- BlockChain/Form1.Designer.cs
+++ BlockChain/Form1.Designer.cs
@@ -43,3 +43,26 @@
             this.AddButton.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.AddButton.Location = new System.Drawing.Point(28, 358);
             this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(502, 30);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(96, 23);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(150, 22);
+            this.dataTextBox.TabIndex = 1;
+            // 
+            // dataLabel
+            // 
+            this.dataLabel.AutoSize = true;
+            this.dataLabel.Location = new System.Drawing.Point(25, 26);
+            this.dataLabel.Name = "dataLabel";
+            this.dataLabel.Size = new System.Drawing.Size(65, 17);
+            this.dataLabel.TabIndex = 2;
+            this.dataLabel.Text = "Данные:";
+            // 
+            // blocksListBox
