089e8141b696cdded8660f05d69722def6247d98
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-04-28 14:33:27 +0000

    initial source

---
+++ BlockChain/Form1.cs
@@ -0,0 +1,28 @@
+
+﻿using System;
+using System.Windows.Forms;
+
+namespace BlockChain
+{
+    public partial class Form1 : Form
+    {
+        private Chain chain;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            chain = new Chain();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            blocksListBox.Items.Clear();
+
+            chain.Add(dataTextBox.Text, userTextBox.Text);
+            blocksListBox.Items.AddRange(chain.Blocks.ToArray());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blocksListBox.Items.AddRange(chain.Blocks.ToArray());
