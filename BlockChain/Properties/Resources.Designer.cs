4c24ebceb54bfa483282bc56bb0253479a44ac82
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-07-14 16:42:43 +0000

    b43: N-PHY: increase initial value of "mind" in RSSI calibration

--- BlockChain/Properties/Resources.Designer.cs
+++ BlockChain/Properties/Resources.Designer.cs
@@ -21,4 +21,4 @@ namespace BlockChain.Properties
     // с параметром /str или заново постройте свой VS-проект.
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-    [global::Sys
+    [global::System.Runtime.CompilerSe
