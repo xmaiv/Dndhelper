785790ec78ece322837290add4f7e7e17b119600
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-07-15 13:49:46 +0000

    Log exception

--- BlockChain/Properties/Resources.Designer.cs
+++ BlockChain/Properties/Resources.Designer.cs
@@ -21,4 +21,5 @@ namespace BlockChain.Properties
     // с параметром /str или заново постройте свой VS-проект.
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-    [global::System.Runtime.CompilerServices.CompilerGenerate
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    
