923cf3f5a1703deb7b49570e997a22d7845eb08b
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-07-16 12:20:02 +0000

    Travis updated

--- BlockChain/Properties/Resources.Designer.cs
+++ BlockChain/Properties/Resources.Designer.cs
@@ -22,4 +22,7 @@ namespace BlockChain.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    
+    internal class Resources
+    {
+
+     
