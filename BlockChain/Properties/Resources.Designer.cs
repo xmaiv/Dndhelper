22f4f6778c84d93d15b5c7fc1a79aef9805e1778
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-08-08 08:15:16 +0000

    rtmutex: Confine deadlock logic to futex

--- BlockChain/Properties/Resources.Designer.cs
+++ BlockChain/Properties/Resources.Designer.cs
@@ -37,4 +37,4 @@ namespace BlockChain.Properties
         /// <summary>
         ///   Возврат кэшированного экземпляра ResourceManager, используемого этим классом.
         /// </summary>
-        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.Comp
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.A
