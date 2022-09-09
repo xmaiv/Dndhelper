da21fe742574aa2b0f1c3a4cf05836d2c9557f02
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-09-09 06:14:50 +0000

    Modify the file

--- BlockChain/Properties/Resources.Designer.cs
+++ BlockChain/Properties/Resources.Designer.cs
@@ -54,4 +54,5 @@ namespace BlockChain.Properties
         /// <summary>
         ///   Переопределяет свойство CurrentUICulture текущего потока для всех
         ///   подстановки ресурсов с помощью этого класса ресурсов со строгим типом.
-        /// </summary
+        /// </summary>
+        [globa
