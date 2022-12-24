523176ddc4ec2658b491d1c771decd95451ca85a
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-12-24 10:02:27 +0000

    Bump to version 0.4.1

--- BlockChainTests/BlockTests.cs
+++ BlockChainTests/BlockTests.cs
@@ -2,3 +2,21 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BlockChain.Tests
+{
+    [TestClass()]
+    public class BlockTests
+    {
+        [TestMethod()]
+        public void SerializeTest()
+        {
+            var block = new Block();
+            var result = block.Serialize();
+            var expected = "{\"Created\":\"\\/Date(1585861200000)\\/\",\"Data\":\"Hello world\",\"Hash\":\"210ae57cbaa1d9dd13894d36969c4407d50bdc5e8181b2f1197a6a6b3abd1eab\",\"PreviousHash\":\"111111\",\"User\":\"Admin\"}";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void DeserializeTest()
+        {
+            var json = "{\"Created\":\"\\/Date(1585861200000)\\/\",\"Data\":\"Hello world\",\"Hash\":\"210ae57cbaa1d9dd13894d36969c4407d50bdc5e8181b2f1197a6a6b3abd1eab\",\"PreviousHash\":\"111111\",\"User\":\"Admin\"}";
