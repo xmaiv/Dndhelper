9b813431d515ed094c0623d545ba38a6b79f41e9
xmaiv <111640278+xmaiv@users.noreply.github.com>
2022-12-25 07:48:32 +0000

    update frontend version

--- BlockChainTests/ChainTests.cs
+++ BlockChainTests/ChainTests.cs
@@ -9,3 +9,22 @@ namespace BlockChain.Tests
         [TestMethod()]
         public void ChainTest()
         {
+            var chain = new Chain();
+            chain.Add("bla bla bla", "Vlad");
+
+            Assert.AreEqual(chain.Last.Data, "bla bla bla");
+            Assert.AreEqual(chain.Last.User, "Vlad");
+        }
+
+        [TestMethod()]
+        public void CheckTest()
+        {
+            var chain = new Chain();
+            chain.Add("bla", "Michael");
+            chain.Add("bla bla", "Alex");
+            chain.Add("bla bla bla", "Tom");
+
+            Assert.IsTrue(chain.Check());
+        }
+    }
+}
