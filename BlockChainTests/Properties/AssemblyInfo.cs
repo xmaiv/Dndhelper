aa15852cfb1659d1e347af22973259533c2f00ed
xmaiv <111640278+xmaiv@users.noreply.github.com>
2023-02-05 09:35:02 +0000

    libceph: SOCK_CLOSED is a flag, not a state

--- BlockChainTests/Properties/AssemblyInfo.cs
+++ BlockChainTests/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка параметра Com
+// Установка параметра ComVisible в значение false де
