7a4034a89c6eb31f6a0c6846d898b2db0978b828
xmaiv <111640278+xmaiv@users.noreply.github.com>
2023-03-23 17:50:17 +0000

    v1.1.3

--- BlockChainTests/Properties/AssemblyInfo.cs
+++ BlockChainTests/Properties/AssemblyInfo.cs
@@ -32,4 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [сборка: AssemblyVersion("1.0.*")]
-[assembly: Assembly
+[assembly: AssemblyVersion("1.0.0.0")]
+[
